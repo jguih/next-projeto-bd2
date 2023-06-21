bc7d5147365131cf77c9c112907c0f763f970578
José Guilherme <gui.kcoal+github@hotmail.com>
2023-06-21 12:41:31 -0300

    Remove spellcheck on game form textarea

--- src/components/ui/textArea.tsx
+++ src/components/ui/textArea.tsx
@@ -9,6 +9,7 @@ export function TextArea({
       required={required}
       placeholder={placeholder}
       value={value}
+      spellCheck={false}
       className={'block mt-2 w-full px-2 py-1 bg-slate-950 border border-slate-600 rounded-md '+ 
         'focus:outline-none focus:border-slate-500 focus:ring-slate-500 focus:ring-2 '+
         'invalid:text-red-600 invalid:border invalid:border-red-600 '+
